1f363539867863635d9adc70acb07a86c992b41e
Gabriel Chartier <gabriel@chartier.link>
2020-09-07 16:26:30 -0400

    Created header and bottomnav components

---
+++ compendia/components/BottomNav.js
@@ -0,0 +1,12 @@
+export default function BottomNav() {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Pull List</li>
+                <li>Collection</li>
+                <li>Search</li>
+            </ul>
+        </nav>
+    );
+}
---
+++ compendia/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Compendia</h1>
+            <img alt="Settings" />
+        </header>
+    );
+}
