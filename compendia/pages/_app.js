ba859c931bb694ee0ce07d15e34b409629de3f5d
Gabriel Chartier <gabriel@chartier.link>
2021-01-06 11:39:23 -0500

    Fixed stash bug

--- compendia/pages/_app.js
+++ compendia/pages/_app.js
@@ -1,22 +1,8 @@
 import "tailwindcss/tailwind.css"
-<<<<<<< Updated upstream
-import { ReactQueryDevtools } from "react-query-devtools"
-import { QueryCache, ReactQueryCacheProvider } from "react-query"
-
-const queryCache = new QueryCache()
-=======
 import { ReactQueryDevtools } from "react-query/devtools"
 import { QueryClient, QueryClientProvider } from "react-query"
 
-// Required imports to prevent errors in any API routes
-// import Publisher from "../database/models/Publisher"
-// import Series from "../database/models/Series"
-// import Comic from "../database/models/Comic"
-// import ComicList from "../database/models/ComicList"
-// import User from "../database/models/User"
-
 const queryClient = new QueryClient()
->>>>>>> Stashed changes
 
 export default function App({ Component, pageProps }) {
     return (
--- compendia/pages/api/releases.js
+++ compendia/pages/api/releases.js
@@ -1,97 +1,3 @@
-<<<<<<< Updated upstream
-const sortByPublisher = JSON.stringify([
-    {
-        id: 1,
-        name: "Image Comics",
-        releases: [
-            {
-                id: 1,
-                title: "#1",
-                cover: "/GideonFalls1.jpg",
-                series: "Gideon Falls",
-            },
-            {
-                id: 2,
-                title: "#1",
-                cover: "/IceCreamMan1.jpg",
-                series: "Ice Cream Man",
-            },
-            {
-                id: 3,
-                title: "#163",
-                cover: "/TheWalkingDead163.png",
-                series: "The Walking Dead",
-            },
-        ],
-    },
-    {
-        id: 2,
-        name: "DC Comics",
-        releases: [
-            {
-                id: 32,
-                title: "#1",
-                cover: "/Flashpoint1.jpg",
-                series: "Flashpoint",
-            },
-            {
-                id: 33,
-                title: "#768",
-                cover: "/WonderWoman768.jpg",
-                series: "Wonder Woman",
-            },
-            {
-                id: 34,
-                title: "#1032",
-                cover: "/DetectiveComics1032.jpg",
-                series: "Detective Comics",
-            },
-            {
-                id: 35,
-                title: "#767",
-                cover: "/TheFlash767.jpg",
-                series: "The Flash",
-            },
-        ],
-    },
-    {
-        id: 3,
-        name: "Marvel Comics",
-        releases: [
-            {
-                id: 20,
-                title: "#39",
-                cover: "/TheAvengers39.jpg",
-                series: "The Avengers: Earth's Mightiest Heroes",
-            },
-            {
-                id: 21,
-                title: "#9",
-                cover: "/StarWars9.jpg",
-                series: "Star Wars - Operation Starlight: The Ancient Relic",
-            },
-            {
-                id: 23,
-                title: "#31",
-                cover: "/Venom31.jpg",
-                series: "Venom: King in Black",
-            },
-            {
-                id: 24,
-                title: "#24",
-                cover: "/CaptainMarvel24.jpg",
-                series: "Captain Marvel",
-            },
-            {
-                id: 26,
-                title: "#16",
-                cover: "/Marauders16.jpg",
-                series: "Marauders",
-            },
-        ],
-    },
-])
-=======
 import dbConnect from "../../database/connection"
 import Publisher from "../../database/models/Publisher"
 import Comic from "../../database/models/Comic"
@@ -115,14 +21,8 @@ dbConnect()
 
 export default async function handler(req, res) {
     const { comicDay } = req.query
->>>>>>> Stashed changes
-
-export default function handler(req, res) {
     res.statusCode = 200
     res.setHeader("Content-Type", "application/json")
-<<<<<<< Updated upstream
-    res.end(sortByPublisher)
-=======
 
     try {
         const releases = await Comic.find({ releaseDate: comicDay }).populate("publisher")
@@ -134,5 +34,4 @@ export default function handler(req, res) {
         console.log(error)
         res.end({ message: error.message })
     }
->>>>>>> Stashed changes
 }
